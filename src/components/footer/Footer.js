e240c5d694c564ee95f10e20c1efc719a52d0780
jonahanderson <jonahmanderson10@gmail.com>
2022-01-29 18:13:43 -0500

    footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+
+function Footer() {
+  return (
+    <div className="footer">
+      Sneaky, aren't you?
+      <div className="link">
+        Check out this site on GitHub:
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
