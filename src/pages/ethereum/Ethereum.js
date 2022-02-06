0d02a2448c8d238921b7b5891b9b401a0353fb22
jonahanderson <jonahmanderson10@gmail.com>
2022-02-06 18:46:19 -0500

    added files

---
+++ src/pages/ethereum/Ethereum.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Ethereum.css';
+
+function Ethereum() {
+    return (
+      <div className="ethereum">
+          lala
+      </div>
+    );
+  }
+  
+  export default Ethereum;
