702887a8e2c21bb993a46501e9f21ab36b85432a
jonahanderson <jonahmanderson10@gmail.com>
2022-09-19 00:56:43 -0400

    contact page with email functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
-import Intro from './components/intro/Intro';
-import About from './components/about/About';
-import Footer from './components/footer/Footer';
+// import Intro from './components/intro/Intro';
+// import About from './components/about/About';
+// import Footer from './components/footer/Footer';
 import Navigation from './components/navigation/Navigation';
 import Contact from './pages/contact/Contact';
 import Experience from './pages/experience/Experience';
-import Posts from './pages/posts/Posts';
+// import Posts from './pages/posts/Posts';
 
 import Homepage from './components/Homepage'
 
@@ -18,10 +18,10 @@ function App() {
       <Router>
         <Navigation></Navigation>
         <Routes>
-          <Route exact path='/' exact element={<Homepage></Homepage>} />
-          <Route exact path='/experience' exact element={<Experience></Experience>} />
-          <Route exact path='/posts' exact element={<Posts></Posts>} />
-          <Route exact path='/contact' exact element={<Contact></Contact>} />
+          <Route exact path='/' element={<Homepage></Homepage>} />
+          <Route exact path='/experience' element={<Experience></Experience>} />
+          {/* <Route exact path='/posts' element={<Posts></Posts>} /> */}
+          <Route exact path='/contact' element={<Contact></Contact>} />
         </Routes>
       </Router>
     </div>  
